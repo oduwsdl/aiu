1f8e24bbde3dbe2de1cf7606a0861d1715a3df83
himarshaj <jayanetti.hr@gmail.com>
2021-02-17 01:38:49 -0500

    Adding three new test cases for testing different levels of collections

--- aiu/nla_collection.py
+++ aiu/nla_collection.py
@@ -60,7 +60,7 @@ def extract_main_collection_data(res):
     #For all levels
     data["name"] = json_data["name"] 
     data["collection_uri"] = json_data["collectionUrl"]     
-    data["description"] = "No description."
+    #data["description"] = "No description."
     data["archived_since"] = "unknown"
     data["archived_until"] = "unknown"
 
@@ -70,16 +70,16 @@ def extract_main_collection_data(res):
 
     if len(subcollections) == 0:
         #This is a subcollection which contains mementos
+        data["collection_type"] = "L3 Subcollection - contains mementos"
         agencies = json_data["agencies"]
-        agency_names = []
-        agency_uris = []
+        #agency_info = []
+        agency = {}
         for p_id in range(0,len(agencies)):
             name = agencies[p_id]["name"]
             uri = agencies[p_id]["url"]
-            agency_names.append(name)
-            agency_uris.append(uri)
-        data["institution_name"] = agency_names
-        data["institution_uri"] = agency_uris
+            agency[name] = uri
+        data["institution_info"] = agency
+        #data["institution_uri"] = agency_uris
         data["subject"] = breadcrumbs[1]["name"] #supercollection name
         data["archived_since"] = json_data["startDate"]["monthyear"] #other formats available
         data["archived_until"] = json_data["endDate"]["monthyear"] #other formats available
@@ -88,10 +88,12 @@ def extract_main_collection_data(res):
         #This is a supercollection or collection which contains subcollections
         if len(breadcrumbs) == 1:
             #This is a supercollection which contains collections
+            data["collection_type"] = "L1 Supercollection - contains collections"
             data["subject"] = data["name"] #same as the collection name
 
         if len(breadcrumbs) == 2:
             #This is a collection which contains subcollections 
+            data["collection_type"] = "L2 Collection - contains subcollections"
             data["subject"] = breadcrumbs[1]["name"] #supercollection name
             data["archived_since"] = json_data["startDate"]["monthyear"] #other formats available
             data["archived_until"] = json_data["endDate"]["monthyear"] #other formats available
@@ -141,4 +143,69 @@ class NLACollection:
 
         #self.logger.debug("exists is set to {}".format(exists))
 
-        return exists          
+        return exists     
+
+    def get_collection_name(self):
+        """Getter for the collection name, as scraped."""
+
+        self.load_collection_metadata()
+
+        name = self.metadata["main"]["name"]
+
+        return name
+
+    def get_collection_uri(self):
+        """Getter for the collection URI, as constructed."""
+
+        self.load_collection_metadata()
+        #https%3A%2F%2Fwebarchive.nla.gov.au%2Fcollection%2F15003
+
+        uri = self.metadata["main"]["collection_uri"]
+
+        return uri
+
+    def get_subject(self):
+        """Getter for the collection's topics, as scraped."""
+        
+        self.load_collection_metadata()
+
+        subjects = self.metadata["main"]["subject"]
+
+        return subjects
+
+    def get_archived_since(self):
+        """Getter for the collection's archived since field, as scraped."""
+
+        self.load_collection_metadata()
+
+        archived_since = self.metadata["main"]["archived_since"]
+
+        return archived_since
+
+
+    def get_archived_until(self):
+        """Getter for the collection's archived since field, as scraped."""
+
+        self.load_collection_metadata()
+
+        archived_since = self.metadata["main"]["archived_until"]
+
+        return archived_since
+
+    def get_collectedby(self):
+        """Getter for the collecting organization's name, as scraped."""
+
+        self.load_collection_metadata()
+        #List of agencies
+        collectedby = self.metadata["main"]["institution_info"]
+
+        return collectedby
+
+    def get_collection_type(self):
+        """Getter for the collecting organization's name, as scraped."""
+
+        self.load_collection_metadata()
+        #List of agencies
+        collectedby = self.metadata["main"]["collection_type"]
+
+        return collectedby  
